0548eb11f69ec287f0fee1c299158b2370f6cdec
Sk Imran <imran@kgec.edu.in>
2017-08-08 22:46:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Home | flatinkolkata.com </title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC|Rancho" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Patrick+Hand|Montserrat" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-default container-fluid" style="background-color:white;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="margin-left:100px; color: black; font-family: 'Rancho', cursive; font-size: 30px;">FlatIn<span style="color: #4EB648">Kolkata.com</span></a>
+    </div>
+  </div>
+</nav>
+<nav class="navbar navbar-default container-fluid" style="margin-top: -22px; background-color: #4EB648;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php" style="color:white;"><b>Home</b><span class="sr-only"></span></a></li>
+        <li><a data-toggle="modal" data-target="#myModal1" style="color:white;"><b>Find Agents</b><span class="sr-only"></span></a></li>
+        <li><a href="#" style="color:white;"><b>Property Views</b></a></li>
+        <li><a data-toggle="modal" data-target="#myModal" style="color:white;"><b>About-Us</b><span class="sr-only"></span></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" style="color:white;">Get Free Agent Advice on : 7596891995</a></li> 
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+	
+<div class="container-fluid" style="margin-top:-21px; width: 100%;"> 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/j.jpg" alt="Los Angeles" style="width:100%; height: 450px;">
+        <div class="row">
+  			<div class="col-xs-6 col-md-3 carousel-caption">
+    		<a class="thumbnail" style="color:#4EB648; text-decoration: none;">
+      		<p>As Kolkatas's #1 real estate agent comparison site, we help over 170,000 Kolkatas research agents every month. Our agency could provide best property deal all  over the kolkata.<br><span style="text-align:center;">Contact-us:7596891995</span></p> 
+    		</a>
+    		<p><a href="#" class="btn btn-primary" role="button">Call:7596891995</a></p>
+  			</div>
+  		</div>
+        <div class="carousel-caption" style="font-family: 'Patrick Hand', cursive;">
+          <h1>Kolkata</h1>
+          <h3>Flat in kolkata</h3>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/g.jpg" alt="Chicago" style="width:100%; height: 450px;">
+        <div class="row">
+  			<div class="col-xs-6 col-md-3 carousel-caption">
+    		<a class="thumbnail" style="color:#4EB648; text-decoration: none;">
+      		<p>As Kolkatas's #1 real estate agent comparison site, we help over 170,000 Kolkatas research agents every month. Our agency could provide best property deal all  over the kolkata.<br><span style="text-align:center;">Contact-us:7596891995</span></p> 
+    		</a>
+    		<p><a href="#" class="btn btn-primary" role="button">Call:7596891995</a></p>
+  			</div>
+  		</div>
+        <div class="carousel-caption" style="font-family: 'Patrick Hand', cursive;">
+          <h1>Salt Lake</h1>
+          <h3>Flat in Salt Lake</h3>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="img/h.jpg" alt="New york" style="width:100%; height: 450px;">
+        <div class="row">
+  			<div class="col-xs-6 col-md-3 carousel-caption">
+    		<a class="thumbnail" style="color:#4EB648; text-decoration: none;">
+      		<p>As Kolkatas's #1 real estate agent comparison site, we help over 170,000 Kolkatas research agents every month. Our agency could provide best property deal all  over the kolkata.<br><span style="text-align:center;">Contact-us:7596891995</span></p> 
+    		</a>
+    		<p><a href="#" class="btn btn-primary" role="button">Call:7596891995</a></p>
+  			</div>
+  			<div class="carousel-caption" style="font-family: 'Patrick Hand', cursive;">
+          <h1>DumDum</h1>
+          <h3>Flat in DumDum</h3>
+        </div>
+  		</div>
+        
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<div class="container" style="margin-top: 20px;">
+	<h1 style=" font-family: 'Montserrat', sans-serif; text-align: center;"><b>Sell for the best price with the least stress</b></h1>
+	<div class="row" style="margin-top:80px;">
+		<div class="col-sm-3">
+			<img src="img/1.png" alt="" style="margin-left: 60px;">
+			<h3 style="color:#4EB648; text-align: center; font-family: 'Montserrat', sans-serif;">We are #1 in kolkata</h3>
+			<p style="text-align: center; font-family: 'Montserrat', sans-serif;">As Kolkatas's #1 real estate agent comparison site, we help over 170,000 Kolkata research agents every month.</p>
+		</div>
+		<div class="col-sm-3">
+			<img src="img/2.png" alt="">
+			<h3 style="color:#4EB648; text-align: center; font-family: 'Montserrat', sans-serif;">Fast and hassle-free</h3>
+			<p style="text-align: center; font-family: 'Montserrat', sans-serif;">We do the research so you don't have to. It takes just 2 minutes to search and request results for your suburb.</p>
+		</div>
+		<div class="col-sm-3">
+			<img src="img/3.png" alt="">
+			<h3 style="color:#4EB648; text-align: center; font-family: 'Montserrat', sans-serif;">100% free, no obligation</h3>
+			<p style="text-align: center; font-family: 'Montserrat', sans-serif;">Our service is 100% free to use. There are no hidden costs or fees, ever. Even our phone consultations are 100% free.</p>
+		</div>
+		<div class="col-sm-3">
+			<img src="img/4.png" alt="">
+			<h3 style="color:#4EB648; text-align: center; font-family: 'Montserrat', sans-serif;">Fair and transparent</h3>
+			<p style="text-align: center; font-family: 'Montserrat', sans-serif;">We analyse more than 30,000 agents in Kolkata based on sales and customer reviews to match you with the right agent.</p>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" style="background-color: #353D45;">
+<h1 style="text-align: center; color: white; font-family: 'Montserrat', sans-serif; margin-top: 45px; ">Get free personalised advice from our friendly team
+</h1>
+<h3 style="text-align: center; color: white; font-family: 'Montserrat', sans-serif; margin-top: 45px;">Simply enter your details below and we’ll give you a call</h3>
+<form class="form-inline" style="text-align: center; margin-top:50px; margin-bottom: 40px;">
+  <div class="form-group">
+    <label for="exampleInputName2"></label>
+    <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail2"></label>
+    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPhone"></label>
+    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="7758945612">
+  </div>
+  <button type="submit" class="btn " style="background-color: #4EB648; color: white;">Request a Call</button>
+</form>
+</div>
+<div class="container-fluid" style="background-color: #F2F2F2; font-family: 'Montserrat', sans-serif;">
+	<div class="row" style="margin-top: 20px; text-align: center;">
+		<div class="col-sm-3">
+			<h5>Top Agents By Your Location</h5>
+			<ul class="nav navbar-nav" style="">
+				<li><a href="#" style="text-decoration: none; color: black;">DumDum</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Salt-lake</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Rajar Hat</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Dharma Tala</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">AgarPara</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Behala</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3">
+			<h5>Top Agents By City</h5>
+			<ul class="nav navbar-nav" style="">
+				<li><a href="#" style="text-decoration: none; color: black;">Kolkata</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">North-24-Pargana</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">West Midnapore</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Bankura</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">Prurlia</a></li>
+				<li><a href="#" style="text-decoration: none; color: black;">South-24-Pargana</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3" style="text-align: center;">
+			<h5>Company</h5>
+			<ul class="nav navbar-nav" style="">
+				<li><a class="navbar-brand" href="#" style="color: black; font-family: 'Rancho', cursive; font-size: 40px;">FlatIn<span style="color: #4EB648">Kolkata.com</span></a></li>
+				
+			</ul>
+		</div>
+		<div class="col-sm-3">
+			<h5>Resources</h5>
+
+      <center><img src="img/facebook.png" alt="">
+        <img src="img/twitter.png" alt="">
+        <img src="img/google-plus.png" alt=""></center>
+		</div>
+
+	</div>
+  <center>&copy;techxorn digital solution</center>
+</div>
+
+<!-- Modal -->
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color:#4EB648;">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel" style="text-align: center; color: white;">About-Us</h4>
+      </div>
+      <div class="modal-body">
+        <p style="text-align: center; font-family: 'Montserrat', sans-serif; font-size: 25px;">We are small group of pepole, build this platfrom to make your dream home 
+        search easier.<br> If you are looking for rent a House/Flat/Office space/Paying Guest(PG),
+        We would love to help you find your dream home.</p>
+        <p style="text-align:right; margin-right: 130px; font-family: 'Montserrat', sans-serif;"></br>Happy Home Searching!!<br>
+        flatinkolkata.com Team.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal" style="background-color:#4EB648; color: white; ">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- second modal -->
+
+<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color:#4EB648;">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel" style="text-align: center; color: white;">Contact Agent</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Phone no</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Phone No">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Required Location</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Enter Location">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Flat<br>
+          <input type="checkbox">PG <br>
+        </label>
+      </div>
+    </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal" style="background-color:#4EB648; color: white; ">Close</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal" style="background-color:#4EB648; color: white; ">Request</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
